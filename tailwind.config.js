de65f325ac88996254971d68c5f3f46a0b5d8cff
Maor <maorbz858@gmail.com>
2024-08-26 15:18:54 +0300

    chore: Add initial files and configurations for Sunnyside project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                screens: {
+                    'desktop-lg': '1680px',
+                    'desktop-md': '1440px',
+                    'desktop-sm': '1280px',
+                    'tablet-lg': '1024px',
+                    'tablet-md': '768px',
+                    'tablet-sm': '640px',
+                    'mobile-lg': '480px',
+                    'mobile-md': '375px',
+                    'mobile-sm': '320px',
+                },
+            },
+            colors: {
+                sunnyside: {
+                    'soft-red': 'hsl(7, 99%, 70%)',
+                    yellow: 'hsl(51, 100%, 49%)',
+                    'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
+                    'dark-blue': 'hsl(198, 62%, 26%)',
+                    'dark-moderate-cyan': 'hsl(168, 34%, 41%)',
+                    'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
+                    'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+                    'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+                    'grayish-blue': 'hsl(210, 4%, 67%)',
+                    white: 'hsl(0, 0%, 100%)',
+                },
+            },
+            fontFamily: {
+                // Sunnyside project fonts
+                barlow: ['Barlow', 'sans-serif'],
+                fraunces: ['Fraunces', 'serif'],
+            },
+        },
+        screens: {
+            // a "desktop-first" approach (larger screens first and smaller screens later)
+            'desktop-lg': { max: '1679px' },
+            'desktop-md': { max: '1439px' },
+            'desktop-sm': { max: '1279px' },
+            'tablet-lg': { max: '1023px' },
+            'tablet-md': { max: '767px' },
+            'tablet-sm': { max: '639px' },
+            'mobile-lg': { max: '479px' },
+            'mobile-md': { max: '374px' },
+            'mobile-sm': { max: '319px' },
+        },
+    },
+    plugins: [],
 };
