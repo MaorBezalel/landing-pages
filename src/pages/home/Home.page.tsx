df0bdc578979450167c5667c4e764b5c05075db1
Maor <maorbz858@gmail.com>
2024-08-30 13:58:49 +0300

    fix: Typo in the heading

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
 
     return (
         <div className="flex w-full flex-col gap-20 p-10">
-            <h1 className="self-center text-6xl text-sunnyside-soft-red">{'Landing Pages (Work In Progress'}</h1>
+            <h1 className="self-center text-6xl text-sunnyside-soft-red">{'Landing Pages (Work In Progress)'}</h1>
             <Link
                 to={ROUTES.SUNNY_SIDE}
                 className="text-cyan-600 underline"
