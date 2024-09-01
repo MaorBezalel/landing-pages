281bbd3814bc8c75395c93cc1599d7b5407deeb6
Maor <maorbz858@gmail.com>
2024-09-01 20:10:16 +0300

    refactor: Update `Main` layout component with increased vertical gap

--- src/pages/loopstudios/layouts/Main.layout.tsx
+++ src/pages/loopstudios/layouts/Main.layout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export function Main({ children }: PropsWithChildren) {
-    return <main className="my-20 flex w-full flex-col gap-20">{children}</main>;
+    return <main className="my-32 flex w-full flex-col gap-32">{children}</main>;
 }
