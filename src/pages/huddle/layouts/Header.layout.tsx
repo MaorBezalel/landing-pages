d5a1a9055cadbd76af2315cdb0de0258afd1956c
Maor <maorbz858@gmail.com>
2024-09-06 16:46:58 +0300

    refactor: Update Header layout component styles

--- src/pages/huddle/layouts/Header.layout.tsx
+++ src/pages/huddle/layouts/Header.layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function Header({ children }: PropsWithChildren) {
     return (
-        <header className="bg-huddle-image-hero-desktop bg-huddle-very-pale-cyan tablet-md:bg-huddle-image-hero-mobile flex w-full flex-col gap-20 bg-center bg-no-repeat py-10 tablet-md:gap-32">
+        <header className="bg-huddle-image-hero-desktop bg-huddle-very-pale-cyan tablet-md:bg-huddle-image-hero-mobile flex w-full flex-col gap-20 bg-center bg-no-repeat py-10 tablet-md:gap-32 tablet-sm:gap-24 mobile-lg:gap-20 mobile-md:gap-16">
             {children}
         </header>
     );
